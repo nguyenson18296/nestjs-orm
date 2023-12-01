538d34fbe9307cc7f3147356c58e52c4fdffe221
son.nguyen <son.nguyen-hoang@gooddata.com>
2023-12-02 01:32:34 +0700

    products

---
+++ src/posts/posts.service.ts
@@ -0,0 +1,60 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { CreatePostDto } from './dto/createPost.dto';
+import { UpdatePostDto } from './dto/updatePost.dto';
+import { Post } from './post.interface';
+import PostEntity from './post.entity';
+
+@Injectable()
+export default class PostsService {
+  private lastPostId = 0;
+  private posts: PostEntity[] = [];
+
+  constructor(
+    @InjectRepository(PostEntity)
+    private postsRepository: Repository<PostEntity>,
+  ) {}
+
+  getAllPosts() {
+    return this.postsRepository.find();
+  }
+
+  async getPostById(id: number) {
+    const post = await this.postsRepository.findOne({
+      where: {
+        id,
+      },
+    });
+    if (post) {
+      return post;
+    }
+    throw new HttpException('Post not found', HttpStatus.NOT_FOUND);
+  }
+
+  async replacePost(id: number, post: UpdatePostDto) {
+    await this.postsRepository.update(id, post);
+    const updatedPost = await this.postsRepository.findOne({
+      where: {
+        id,
+      },
+    });
+    if (updatedPost) {
+      return updatedPost;
+    }
+    throw new HttpException('Post not found', HttpStatus.NOT_FOUND);
+  }
+
+  async createPost(post: CreatePostDto) {
+    const newPost = await this.postsRepository.create(post);
+    await this.postsRepository.save(newPost);
+  }
+
+  async deletePost(id: number) {
+    const deletePost = await this.postsRepository.delete(id);
+    if (!deletePost.affected) {
+      throw new HttpException('Post not found', HttpStatus.NOT_FOUND);
+    }
+  }
+}
